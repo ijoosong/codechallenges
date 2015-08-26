61f41a1a7ca1fef7cf51d0dd6907e47524929c7f
joe song <ijoosong@gmail.com>
2015-08-26 09:54:46 -0400

    finishing up

---
+++ toam/challenge3_test.py
@@ -0,0 +1,12 @@
+import unittest
+from challenge3 import incr_dict
+
+class DictTestCase(unittest.TestCase):
+    '''Tests for incr_dict'''
+    def test_one_element_tuple_with_empty_dict(self):
+        '''should return {'a':1}'''
+        self.assertEqual(incr_dict({}, ('a')), {'a':1})
+
+    def test_multiple_element_tuple_with_empty_dict(self):
+        '''should return {'a':{'b'{'c':1}}}'''
+        self.assertEqual(incr_dict({}, ('a', 'b', 'c')), {'a':{'b':{'c':1}}})
