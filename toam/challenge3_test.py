6f8c97b2cedba996f8b68641800cae7f2fc7337c
joe song <ijoosong@gmail.com>
2015-08-26 10:27:45 -0400

    fixed tests

--- toam/challenge3_test.py
+++ toam/challenge3_test.py
@@ -6,12 +6,12 @@ class DictTestCase(unittest.TestCase):
     def test_one_element_tuple_with_empty_dict(self):
         '''should return {'a':1}'''
         tup = ('a')
-        self.assertEqual(incr_dict({}, tup, {'a':1})
+        self.assertEqual(incr_dict({}, tup), {'a':1})
 
     def test_multiple_element_tuple_with_empty_dict(self):
         '''should return {'a':{'b'{'c':1}}}'''
         tup = ('a', 'b', 'c')
-        self.assertEqual(incr_dict({}, tup, {'a':{'b':{'c':1}}})
+        self.assertEqual(incr_dict({}, tup), {'a':{'b':{'c':1}}})
 
     def test_one_element_tuple_with_prexisting_dict(self):
         '''should return {'a':{'b':{'c':1}}, 'd':1}'''
