101a9b6b7fa57e47532a02eb865fb8afb6b7d6f0
joe song <ijoosong@gmail.com>
2015-08-26 10:09:21 -0400

    more texts

--- toam/challenge3.py
+++ toam/challenge3.py
@@ -1,4 +1,5 @@
 def incr_dict(dct, tup):
+    '''given a dct and a tup, iterates through tuple to increment the last element of the tuple in a nested dictionary.  calls each element a node until last leaf'''
     current_dct = dct
     for ele in tup:
         if ele == tup[-1]:
@@ -9,6 +10,7 @@ def incr_dict(dct, tup):
     return dct
 
 def increment(current_dct, ele):
+    '''increments the last element. if not available, make equal to 1. else, increment'''
     if ele not in current_dct:
         current_dct[ele] = 1
     else:
--- toam/challenge3_test.py
+++ toam/challenge3_test.py
@@ -5,11 +5,21 @@ class DictTestCase(unittest.TestCase):
     '''Tests for incr_dict'''
     def test_one_element_tuple_with_empty_dict(self):
         '''should return {'a':1}'''
-        self.assertEqual(incr_dict({}, ('a')), {'a':1})
+        tup = ('a')
+        self.assertEqual(incr_dict({}, tup, {'a':1})
 
     def test_multiple_element_tuple_with_empty_dict(self):
         '''should return {'a':{'b'{'c':1}}}'''
-        self.assertEqual(incr_dict({}, ('a', 'b', 'c')), {'a':{'b':{'c':1}}})
+        tup = ('a', 'b', 'c')
+        self.assertEqual(incr_dict({}, tup, {'a':{'b':{'c':1}}})
 
+    def test_one_element_tuple_with_prexisting_dict(self):
+        '''should return {'a':{'b':{'c':1}}, 'd':1}'''
+        dct = {'a':{'b':{'c':1}}}
+        tup = ('d')
+        self.assertEqual(incr_dict(dct, tup), {'a':{'b':{'c':1}}, 'd':1})
+
+    def test_multiple_element_tuple_with_prexisting_dict(self):
+        
 if __name__ == '__main__':
     unittest.main()
