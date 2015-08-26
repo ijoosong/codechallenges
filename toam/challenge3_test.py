41daf0b04cf558216265b51950a320d89e1004f4
joe song <ijoosong@gmail.com>
2015-08-26 10:46:50 -0400

    more tests

--- toam/challenge3_test.py
+++ toam/challenge3_test.py
@@ -25,6 +25,28 @@ class DictTestCase(unittest.TestCase):
         tup = ('a', 'b', 'c')
         self.assertEqual(incr_dict(dct, tup), {'a':{'b':{'c':2}}})
 
-                         
+    def test_multiple_element_tuple_that_partially_overlaps_with_prexisting_dict(self):
+        '''should return {'a':{'b':{'c':1, 'd':1}}}'''
+        dct = {'a':{'b':{'c':1}}}
+        tup = ('a', 'b', 'd')
+        self.assertEqual(incr_dict(dct, tup), {'a':{'b':{'c':1, 'd':1}}})
+
+    def test_multiple_element_tuple_that_has_no_overlaps_with_prexisting_dict(self):
+        '''should return {'a':{'b':{'c':1}}, 'x':{'y':{'z':1
+        }}}'''
+        dct = {'a':{'b':{'c':1}}}
+        tup = ('x','y','z')
+        self.assertEqual(incr_dict(dct, tup), {'a':{'b':{'c':1}},'x':{'y':{'z':1}}})
+
+    def test_one_element_tuple_with_prexisting_dict(self):
+        '''should return {'a':{'b':{'c':1}}, 'd':1}'''
+        dct = {'a':{'b':{'c':1}}}
+        tup = ('d')
+        self.assertEqual(incr_dict(dct, tup), {'a':{'b':{'c':1}}, 'd':1})
+
+    def test_tuples_that_arent_strings(self):
+        dct = {}
+        tup = (1, 2, 3)
+        self.assertEqual(incr_dict(dct, tup), {1:{2:{3:1}}})
 if __name__ == '__main__':
     unittest.main()
