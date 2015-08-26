2a4777cf9cf1488bca4abf5681a1d5807a69cec9
joe song <ijoosong@gmail.com>
2015-08-26 02:52:44 -0400

    stuff

--- toam/try2.py
+++ toam/try2.py
@@ -1,18 +1,16 @@
-def idct(dct, tup):
+def incr_dict(dct, tup):
     current_dct = dct
     for ele in tup:
-        print ele
         if ele == tup[-1]:
-            if ele not in current_dct:
-                current_dct[ele] = 1
-            else:
-                current_dct[ele] += 1
+            increment(current_dct, ele)
         elif ele not in current_dct:
             current_dct[ele] = {}
-            print 'inside if statement'
-            print current_dct
         current_dct = current_dct[ele]
-        print 'outside if statement'
-        print current_dct
-        print dct
     return dct
+
+def increment(current_dct, ele):
+    if ele not in current_dct:
+        current_dct[ele] = 1
+    else:
+        current_dct[ele] += 1
+
